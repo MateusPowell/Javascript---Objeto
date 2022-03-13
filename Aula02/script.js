5b5da99901c53c48eec7b0b53de4f33333066654
Mateus Powell <mkabyz@gmail.com>
2022-03-13 11:36:23 -0300

    tv

--- Aula02/script.js
+++ Aula02/script.js
@@ -1,66 +1,24 @@
-// const pessoa = {
-//     nome: "Jaque",
-//     idade: 27,
-//     cumprimentaPessoa (nome = "Fulano"){
-//          return console.log("seja bem vindos: " + nome);
-//     },    
-// };
+console.log("==== TESTE ===")
 
-// const mensagem = pessoa.cumprimentaPessoa("Mateus");
-
-
-// const counter = {
-//     count: 0,
-//     next: function () {
-//         return this.count++;
-//     },
-// }
-// counter.next()
-// counter.next()
-// counter.next()
-// counter.next()
-
-// console.log(counter.count);
-
-// function show() {
-//     console.log (this == window)
-// }
-
-// show()
-
-// window.show()
-
-// console.log(window)
-
-// let carro = {
-//     marca: "Hyundai",
-//     getMarca: function() {
-//         return this.marca;
-//     }
-// }
-// console.log(carro.getMarca());
-
-// let marca = carro.getMarca();
-// console.log(marca);
-
-// let marca1 = carro.getMarca.bind(carro);
-// console.log(marca1())
-
-// console.log(carro.getMarca.bind)
-
-// chamada 
-
-function Carro(placa, marca) {
-    this.placa = placa;
-    this.marca = marca;
+const televisao = {
+    canal: "null",
+    volume: 0,
+    ligada: false,
+    ligar() {
+        this.ligada = true;
+        return "ligada"
+    },
+    desligar(){
+        this.ligada = false
+        return "desligada"
+    },
+    mudaDeCanal(i){
+        this.canal = i 
+    },
+    aumentaVolume() {
+        this.volume = this.volume+1;   
+    },
+    diminuiVolume(){
+        this.volume = this.volume-1;
+    }
 }
-// criar uma função dentro da funão construtora carro
-
-Carro.prototype.getplaca = function () {
-    return this.getplaca;
-}
-
-let carro1 = new Carro ("ddx2118", "bmw")
-
-console.log(carro1)
-
