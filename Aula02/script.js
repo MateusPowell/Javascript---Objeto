bd9619487b98c6eb12b96895658e77835b59cd74
Mateus Powell <mkabyz@gmail.com>
2022-03-13 20:25:22 -0300

    televisao class

--- Aula02/script.js
+++ Aula02/script.js
@@ -1,6 +1,8 @@
 console.log("==== TESTE ===")
 
-const televisao = {
+// Televisao normal
+
+/* const televisao = {
     canal: "null",
     volume: 0,
     ligada: false,
@@ -21,4 +23,50 @@ const televisao = {
     diminuiVolume(){
         this.volume = this.volume-1;
     }
+}*/
+
+// Televisao Construtora
+
+/* function televisao (canal,volume,ligada=false){
+    this.canal = canal,
+    this.volume = volume,
+    this.ligada = ligada
 }
+
+const assistindo = new televisao("20", 10,true)
+const assistindo2= new televisao("20", 10)
+console.log(assistindo)
+console.log(assistindo2)*/
+
+// televisao class
+
+class televisao {
+
+    constructor(canal, volume){
+        this.canal = canal;
+        this.volume = volume;
+        this.ligado = false;        
+    }
+    ligar(){
+        this.ligado = true
+        console.log("ligada")
+    }
+    delisgar(){
+        this.ligado = false
+        console.log("desligada")
+    }
+    mudaDeCanal(i){
+        this.canal = i 
+    }
+    aumentaVolume() {
+        this.volume = this.volume+1; 
+        console.log("volume " + this.volume)  
+    }
+    diminuiVolume(){
+        this.volume = this.volume-1;        
+        console.log("volume " + this.volume)
+    }
+}
+
+const tele = new televisao(5,10)
+
